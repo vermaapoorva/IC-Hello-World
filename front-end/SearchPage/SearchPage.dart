7e00d9f8ec4f5999f8947945ad0deabc065ac57d
ras19 <ras19@ic.ac.uk>
2021-03-27 21:31:04 +0000

    Removed dummy app in SearchPage.dart

--- front-end/SearchPage/SearchPage.dart
+++ front-end/SearchPage/SearchPage.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SearchPage(),
-    );
-  }
-}
-
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
