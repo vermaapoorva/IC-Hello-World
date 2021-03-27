6af248a1c5122a477674813fb12006407fc7cea8
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-03-27 17:02:52 +0000

    Login Page implemented

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,101 +23,124 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Goals'),
+      // home: LoginScreen(title: 'Goals'),
+      home: LoginScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// class LoginScreen extends StatefulWidget {
+//   LoginScreen({Key key, this.title}) : super(key: key);
+//
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+//
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+//
+//   final String title;
+//
+//   @override
+//   // _MyHomePageState createState() => _MyHomePageState();
+//   _LoginScreen createState() => _LoginScreen();
+// }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextStyle style = TextStyle(fontFamily: 'Lato', fontSize: 20.0);
-
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final emailField = TextField(
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
-          border:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-    );
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
-    return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.blueAccent,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "images/lake.jpg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(
-                  height: 35.0,
-                ),
-                loginButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return FlutterLogin(
+      title: 'Tether',
+      // logo: 'images/lake.jpg',
+      // onLogin: _authUser,
+      // onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        // Navigator.of(context).pushReplacement(MaterialPageRoute(
+        //   builder: (context) => DashboardScreen(),
+        // ));
+      },
+      // onRecoverPassword: _recoverPassword,
     );
   }
 }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   TextStyle style = TextStyle(fontFamily: 'Lato', fontSize: 20.0);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     final emailField = TextField(
+//       obscureText: false,
+//       style: style,
+//       decoration: InputDecoration(
+//           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+//           hintText: "Email",
+//           hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+//           border:
+//           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+//     );
+//     final passwordField = TextField(
+//       obscureText: true,
+//       style: style.copyWith(
+//           color: Colors.white, fontWeight: FontWeight.bold),
+//       decoration: InputDecoration(
+//           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+//           hintText: "Password",
+//           hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+//           border:
+//           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+//     );
+//     final loginButon = Material(
+//       elevation: 5.0,
+//       borderRadius: BorderRadius.circular(30.0),
+//       color: Color(0xff01A0C7),
+//       child: MaterialButton(
+//         minWidth: MediaQuery.of(context).size.width,
+//         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+//         onPressed: () {},
+//         child: Text("Login",
+//             textAlign: TextAlign.center,
+//             style: style.copyWith(
+//                 color: Colors.white, fontWeight: FontWeight.bold)),
+//       ),
+//     );
+//
+//     return Scaffold(
+//       body: Center(
+//         child: Container(
+//           color: Colors.blueAccent,
+//           child: Padding(
+//             padding: const EdgeInsets.all(36.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 SizedBox(
+//                   height: 155.0,
+//                   child: Image.asset(
+//                     "images/lake.jpg",
+//                     fit: BoxFit.contain,
+//                   ),
+//                 ),
+//                 SizedBox(height: 45.0),
+//                 emailField,
+//                 SizedBox(height: 25.0),
+//                 passwordField,
+//                 SizedBox(
+//                   height: 35.0,
+//                 ),
+//                 loginButon,
+//                 SizedBox(
+//                   height: 15.0,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
