a20932a0bf480105f980d57d3211602ffe902ed6
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-03-27 20:14:31 +0000

    fixed login and added bubble navbar

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
-import 'HomeScreen/HomeScreen.dart';
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+// import 'HomeScreen/HomeScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tether',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: LoginScreen(title: 'Goals'),
-      home: LoginScreen(),
+      home: MyHomePage(title: 'Goals'),
     );
   }
 }
@@ -50,17 +51,17 @@ class MyApp extends StatelessWidget {
 // }
 
 class LoginScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'Tether',
       // logo: 'images/lake.jpg',
-      // onLogin: _authUser,
-      // onSignup: _authUser,
       emailValidator: invalidUsername,
+      passwordValidator: invalidPassword,
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => HomeScreen(),
+          builder: (context) => MyHomePage(),
         ));
       },
       messages: LoginMessages(
@@ -71,7 +72,114 @@ class LoginScreen extends StatelessWidget {
   }
 
   String invalidUsername(String a) {
-    return "Invalid username";
+    if (a == "pass") {
+      print("Username accepted");
+      return null;
+    }
+    return "Invalid Username";
+  }
+
+  String invalidPassword(String a) {
+    if (a == "pass") {
+      print("Password accepted");
+      return null;
+    }
+    return "Invalid Password";
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentIndex = 0;
+  }
+
+  void changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(
+                16)), //border radius doesn't work when the notch is enabled.
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.red,
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.red,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.account_circle,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.account_circle,
+                color: Colors.deepPurple,
+              ),
+              title: Text("Account")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.search,
+                color: Colors.indigo,
+              ),
+              title: Text("Search")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.group,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.group,
+                color: Colors.green,
+              ),
+              title: Text("Groups"))
+        ],
+      ),
+    );
   }
 }
 
