44db0c5da8a2ac3f2add0786a6587cbc885e0865
Rohan Gupta <guptarohan@hotmail.co.uk>
2021-03-28 09:44:31 +0100

    Merged and added http imports

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'pages/HomePage.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -29,25 +30,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class LoginScreen extends StatefulWidget {
-//   LoginScreen({Key key, this.title}) : super(key: key);
-//
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-//
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-//
-//   final String title;
-//
-//   @override
-//   // _MyHomePageState createState() => _MyHomePageState();
-//   _LoginScreen createState() => _LoginScreen();
-// }
 
 class LoginScreen extends StatelessWidget {
 
