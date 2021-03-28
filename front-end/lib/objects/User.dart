840b74e2aa4e7121c50018b5b265e20e6a863b41
Alex Usher <awu19@ic.ac.uk>
2021-03-28 10:12:18 +0100

    add classes for Auth, User in the app code

---
+++ front-end/lib/objects/User.dart
@@ -0,0 +1,29 @@
+class User {
+  String userid;
+  String name;
+  String username;
+  String email;
+
+  User(String userid, String name, String username, String email) {
+    this.userid = userid;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+  }
+
+  String getUserId() {
+    return userid;
+  }
+
+  String getName() {
+    return name;
+  }
+
+  String username() {
+    return username;
+  }
+
+  String email() {
+    return email;
+  }
+}
